1612e9d9994e42ffce98e3fedce2c56b2a4b58f8
7shoaib7 <69447663+7shoaib7@users.noreply.github.com>
2022-10-22 23:16:32 +0530

    logo-background-card-detail

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/images/wobot-logo.svg';
+import cardLogo from './assets/images/Solid_lines.svg';
+import { useState } from "react"
 
 function App() {
+  const [companySize, setCompanySize] = useState(["1-20", "21-50", "51-200", "201-500", "500+"])
+  const [size, setSize] = useState("");
+  const handleCompanySize = (item) => {
+    setSize(item)
+    //  console.log(item)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="logo-box">
+        <img className="logo-img" src={logo} alt="logo" />
+      </div>
+      <div className="box-rotate-fixed">
+        <div className="box-strip box-strip-middle"></div>
+        <div className="box-strip box-strip-top-right"></div>
+        <div className="box-strip box-strip-bottom-left"></div>
+      </div>
+
+      <div className="card">
+        <div className="card-details">
+          <div className="card-heading">
+            <div className="card-logo">
+              <img src={cardLogo} alt="cardLogo" />
+            </div>
+            <h3 className="card-title">It's a delight to have you onboard</h3>
+            <p className="card-sub-title">Help us know you better.</p>
+            <p className="card-sub-title">(This is how we optimize Wobot as per your business needs)</p>
+          </div>
+
+          <div className="card-form">
+            <form className="card-form-details">
+              <div className="form-group">
+                <label className="form-label">Company name</label>
+                <input type="text" className="form-control " placeholder="e.g. Example Inc" />
+              </div>
+              <div className="form-group">
+                <label className="form-label">Industry</label>
+                <select className="form-control">
+                  <option value="Option0">Select</option>
+                  <option value="Option1">Option1</option>
+                </select>
+              </div>
+              <div className="form-group">
+                <label className="form-label">Company size</label>
+                {companySize.map((item) => (
+                  <div className="company-size gap" onClick={() => handleCompanySize(item)}>
+                    {item}
+                  </div>
+                ))}
+              </div>
+            </form>
+          </div>
+
+          <button type="submit" className="btn btn-primary btn-block">Get Started</button>
+        </div>
+      </div>
+
     </div>
   );
 }
